bb18e04e6eeeb1437e05e233973c30db8951ee74
SONG <simpledeveloper.songs@gmail.com>
2021-11-22 09:11:56 +0900

    get tags in form of a list

--- domain/crawler.py
+++ domain/crawler.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 from utils.data_structures import Node
-import json
 
 BASE_URL = "https://github.com"
 
@@ -23,8 +22,6 @@ def make_file_tree(prev_url, url, node):
             ref = a["href"]
             if prev_url != ref and not ref.endswith('.md'):
                 print(ref)
-
-                tree[prev_url].append({ref: []})
                 child = Node(ref)
                 node.children.append(child)
                 make_file_tree(url, ref, child)
@@ -32,8 +29,6 @@ def make_file_tree(prev_url, url, node):
             elif url != start_url and ref.endswith('.md'):
                 child = Node(ref)
                 node.children.append(child)
-
-                tree[start_url][-1][url].append(ref)
                 print(ref)
 
 
@@ -48,32 +43,28 @@ def get_table(url):
 
         try:
             tag_table = soup.find("table")
-            print(tag_table.find("thead").find("tr").find("th").text)
-            print(tag_table.find("tbody").findAll("tr"))
+            return [i.text for i in tag_table.find("tbody").findAll("tr")]
         except:
             print("tag 없음")
-        finally:
-            print("\n")
+
+        return None
 
 
 def dfs(node):
-    print(node.data)
     if len(node.children) == 0:
-        get_table(node.data)
+        print(node.data)
+        tag = get_table(node.data)
+        print(tag)
     for i in node.children:
         dfs(i)
 
 
 start_url = '/SHSongs/fast-paper'
-tree = {start_url: []}
 root = Node(start_url)
 
 make_file_tree(start_url, start_url, root)
-print(tree)
 
 print("\n\n")
 
-j = json.dumps(tree, indent=4, sort_keys=True)
-print(j)
 
 dfs(root)
