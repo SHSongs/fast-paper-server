0800c92782ab4731c7b2e80d32b1bdafad7204cd
SONG <simpledeveloper.songs@gmail.com>
2021-12-17 15:02:04 +0900

    안쓰는 file, import 삭제

--- db/models.py
+++ db/models.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Text
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, ForeignKey, Integer, Text
 
 from database import Base
 
